1ef74acf2eec264a4af2742b581bb70b1590ef05
Ragala-Orjouane <orjouaneragala1@gmail.com>
2026-03-05 14:24:14 +0000

    feat: implement categorie viewset with drf

--- pharma_backend/apps/categories/views.py
+++ pharma_backend/apps/categories/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema
+
+from .models import Categorie
+from .serializers import CategorieSerializer
+
+
+@extend_schema(tags=["Categories"])
+class CategorieViewSet(viewsets.ModelViewSet):
+    """
+    API permettant de gérer les catégories de médicaments.
+    """
+
+    queryset = Categorie.objects.all()
+    serializer_class = CategorieSerializer

cd0eb8d4f4a3a25b9349327bf51c8d01eea7980a
Ragala-Orjouane <orjouaneragala1@gmail.com>
2026-03-05 14:05:11 +0000

    feat: add categorie model with constraints and migration

--- pharma_backend/apps/categories/models.py
+++ pharma_backend/apps/categories/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class Categorie(models.Model):
+    """
+    Représente une catégorie de médicaments.
+    """
+
+    nom = models.CharField(
+        max_length=150,
+        unique=True,
+        verbose_name="Nom"
+    )
+    description = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Description"
+    )
+    date_creation = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['nom']
+        verbose_name = "Catégorie"
+        verbose_name_plural = "Catégories"
+
+    def __str__(self):
+        return self.nom
--- pharma_backend/config/settings/base.py
+++ pharma_backend/config/settings/base.py
@@ -13,12 +13,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-import decouple as config
+from decouple import config
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 

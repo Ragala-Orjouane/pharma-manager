1545adc1950083d66603ab942624c1272aeb12cc
Ragala-Orjouane <orjouaneragala1@gmail.com>
2026-03-06 10:42:37 +0000

    feat: add medicament serializer with business validation

---
+++ pharma_backend/apps/medicaments/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Medicament
+
+
+class MedicamentSerializer(serializers.ModelSerializer):
+    """
+    Serializer pour le modèle Medicament.
+    """
+
+    est_en_alerte = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Medicament
+        fields = "__all__"
+
+    def validate(self, data):
+        """
+        Vérifie cohérence prix et stock.
+        """
+
+        if data["prix_vente"] < data["prix_achat"]:
+            raise serializers.ValidationError(
+                "Le prix de vente ne peut pas être inférieur au prix d'achat."
+            )
+
+        return data

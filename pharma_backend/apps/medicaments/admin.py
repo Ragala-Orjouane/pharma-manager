34a2ac88cd4c233ec2070e52d14bd79826262cc6
Ragala-Orjouane <orjouaneragala1@gmail.com>
2026-03-06 10:40:43 +0000

    feat: register medicament model in django admin

--- pharma_backend/apps/medicaments/admin.py
+++ pharma_backend/apps/medicaments/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Medicament
 
 # Register your models here.
+@admin.register(Medicament)
+class MedicamentAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "nom",
+        "categorie",
+        "stock_actuel",
+        "stock_minimum",
+        "prix_vente",
+        "date_expiration",
+    )
+
+    list_filter = (
+        "categorie",
+        "ordonnance_requise",
+    )
+
+    search_fields = (
+        "nom",
+        "dci",
+    )

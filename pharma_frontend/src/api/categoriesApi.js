7f4747197d8f71c260e1f27553aaea40dd78388d
Ragala-Orjouane <orjouaneragala1@gmail.com>
2026-03-07 12:06:51 +0000

    feat(api): add categories api

---
+++ pharma_frontend/src/api/categoriesApi.js
@@ -0,0 +1,11 @@
+import axiosInstance from "./axiosConfig";
+
+export const getCategories = async () => {
+  const response = await axiosInstance.get("/categories/");
+  return response.data.results || response.data;
+};
+
+export const createCategory = async (data) => {
+  const response = await axiosInstance.post("/categories/", data);
+  return response.data;
+};

9ce0ff896a6518f33563c7393618336c669d5982
Ragala-Orjouane <orjouaneragala1@gmail.com>
2026-03-07 11:58:08 +0000

    feat(medicaments): update medicament serializer

--- pharma_backend/apps/medicaments/serializers.py
+++ pharma_backend/apps/medicaments/serializers.py
@@ -19,8 +19,8 @@ class MedicamentSerializer(serializers.ModelSerializer):
         """
 
         if data["prix_vente"] < data["prix_achat"]:
-            raise serializers.ValidationError(
-                "Le prix de vente ne peut pas être inférieur au prix d'achat."
-            )
+            raise serializers.ValidationError({
+                "prix_vente":"Le prix de vente ne peut pas être inférieur au prix d'achat."
+            })
 
         return data
